c842aaa349a74d2f6d3b1f6edae05db96895493a
Rasputin <TensorMouse@gmail.com>
2022-10-03 23:37:47 +0200

    reworked tracking logic
    improve the tracking logic soon

--- workout-tracker.py
+++ workout-tracker.py
@@ -1,35 +1,53 @@
 import pandas as pd
 import numpy as np
 
+def load_workout_history(path='workout_history.csv'):
+    try:
+        workout=pd.read_csv('workout_history.csv').T
+    except FileNotFoundError as e:
+        workout=pd.DataFrame()
+    return(workout)
+
+def enter_stats():
+    weight=input('input weight, s for stop, enter for 6: ') or "6"
+    if weight == 's':
+        return([np.nan,np.nan,np.nan])
+    
+    reps=input('input reps, s for stop, enter for 10: ') or "10"
+    if weight == 's':
+        return([weight,np.nan,np.nan])
+    
+    rest=input('input rest, s for stop, enter for 60 ') or "60"
+    if weight == 's':
+        return([weight,reps,np.nan])
+    
+    return([weight,reps,rest])
+
+
+
+
 if __name__ == "__main__":
     workout = pd.DataFrame()
     while True:
-        try:
-            workout=pd.read_csv('workout_history.csv').T
-        except FileNotFoundError as e:
-            workout=pd.DataFrame()
+        workout = load_workout_history()
         workout_part=pd.Series()
         workout_part['exercise'] = input('input exercise: ')
         if workout_part['exercise']=='stop':
             break
-
         workout_part['date'] = input('input date: ')
         set_number=0
         while True:
             set_number+=1
-            workout_part['set '+str(set_number)+' weight']=input('input weight: ')
-            if workout_part['set '+str(set_number)+' weight'] == 'stop':
-                workout_part['set '+str(set_number)+' weight'] = np.nan
+            workout_stats=enter_stats()
+            if workout_stats[0]==np.nan:
                 break
-            workout_part['set '+str(set_number)+' reps']=input('input reps: ')
-            if workout_part['set '+str(set_number)+' reps'] == 'stop':
-                workout_part['set '+str(set_number)+' reps'] == np.nan
+            workout_part['set '+str(set_number)+': weight']=workout_stats[0]
+            workout_part['set '+str(set_number)+': reps']=workout_stats[1]
+            workout_part['set '+str(set_number)+': rest']=workout_stats[2]
+            workout=pd.concat([workout,workout_part],axis=1)
+            print(workout_stats)
+            if np.nan in workout_stats:
                 break
-            workout_part['set '+str(set_number)+' rest']=input('input rest: ')
-            if workout_part['set '+str(set_number)+' rest'] == 'stop':
-               workout_part['set '+str(set_number)+' rest'] = np.nan
-               break
-        workout=pd.concat([workout,workout_part],axis=1)
         workout.T.to_csv('workout_history.csv',index=False)
         print(workout)
 
