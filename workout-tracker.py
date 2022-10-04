2421d8c16adf35c4200c1ddfbe2242974400b092
Rasputin <TensorMouse@gmail.com>
2022-10-05 00:51:26 +0200

    can abort in the middle of a set now
    make old default value logic work properly

--- workout-tracker.py
+++ workout-tracker.py
@@ -8,17 +8,18 @@ def load_workout_history(path='workout_history.csv'):
         workout=pd.DataFrame()
     return(workout)
 
-def enter_stats():
+def enter_stats(last_exercise,set_number):
+
     weight=input('input weight, s for stop, enter for 6: ') or "6"
     if weight == 's':
         return([np.nan,np.nan,np.nan])
     
     reps=input('input reps, s for stop, enter for 10: ') or "10"
-    if weight == 's':
+    if reps == 's':
         return([weight,np.nan,np.nan])
     
     rest=input('input rest, s for stop, enter for 60 ') or "60"
-    if weight == 's':
+    if rest == 's':
         return([weight,reps,np.nan])
     
     return([weight,reps,rest])
@@ -31,23 +32,29 @@ if __name__ == "__main__":
     while True:
         workout = load_workout_history()
         workout_part=pd.Series()
-        workout_part['exercise'] = input('input exercise: ')
+        print(workout)
+        exercise_name = input('input exercise: ')
+        workout_part['exercise'] = exercise_name
         if workout_part['exercise']=='stop':
             break
+        last_exercised=workout.T['exercise']
+        index_e=last_exercised.where(last_exercised==exercise_name).last_valid_index()
+        last_exercise=workout[index_e]
         workout_part['date'] = input('input date: ')
         set_number=0
         while True:
             set_number+=1
-            workout_stats=enter_stats()
+            workout_stats=enter_stats(last_exercise,set_numer)
             if workout_stats[0]==np.nan:
                 break
             workout_part['set '+str(set_number)+': weight']=workout_stats[0]
             workout_part['set '+str(set_number)+': reps']=workout_stats[1]
             workout_part['set '+str(set_number)+': rest']=workout_stats[2]
-            workout=pd.concat([workout,workout_part],axis=1)
             print(workout_stats)
             if np.nan in workout_stats:
+                workout=pd.concat([workout,workout_part],axis=1)
                 break
-        workout.T.to_csv('workout_history.csv',index=False)
+
+        workout.T.sort_values(by=['date']).to_csv('workout_history.csv',index=False)
         print(workout)
 
